d63c86a5b7ab2109b127f1f21673ad5d9e0c1533
Benjamin Zarate <benja_zarate@hotmail.es>
2022-09-14 21:30:29 -0300

    Entrega eventos

--- Simulator/buy.js
+++ Simulator/buy.js
@@ -1,7 +1,15 @@
 let envioTotal = 0; //costos de envios, este aumentara segun la cantidad de productos
 let total = 0; //Precio total a gastar
+
+//Contenedor para mostrar el carrito de compras
 const contenedor = document.createElement("div");
 
+//ID de los inputs para la cantidad de libretas
+var input1 = 'amount1';
+let input2 = 'amount2';
+let input3 = 'amount3';
+
+//Lista de productos en el carrito de compras
 const listProducts = [];
 
 class Notebook
@@ -22,17 +30,18 @@ const _notebook1 = new Notebook("Libreta horizontal", 10000, 3000);
 const _notebook2 = new Notebook("Libreta horizontal multicolor", 12000, 3000);
 const _notebook3 = new Notebook("Libreta blanca y rosa", 11000, 3000);
 
-
+//Se asignan los eventos para los botones de añadir carrito apra cada libreta
 document.getElementById('notebook1').addEventListener('click', function() {
-    NotebookAmount(_notebook1);
+    NotebookAmount(_notebook1, input1);
 }, false);
 
 document.getElementById("notebook2").addEventListener("click", function() {
-    NotebookAmount(_notebook2);
+    NotebookAmount(_notebook2, input2);
 }, false);
 document.getElementById("notebook3").addEventListener("click", function() {
-    NotebookAmount(_notebook3);
+    NotebookAmount(_notebook3, input3);
 }, false);
+
 
 function AddNotebook(notebook)
 {
@@ -42,19 +51,21 @@ function AddNotebook(notebook)
     listProducts.push(notebook);
 }
 
-function NotebookAmount(notebook)
+//Se recoge la cantidad de libretas añadidas por el usuario y se suman a la lista de productos
+//Se muestra una alerta de que fueron añadidos existosa mente
+//Se muestran los productos en pantalla
+function NotebookAmount(notebook, input)
 {
-    contenedor.remove();
-    let amount = prompt("¿Cuantas libretas desea?");
+    var amount = document.getElementById(input).value;
     for(let i = 0; i < amount; i++)
     {
         AddNotebook(notebook);
-        
     } 
     alert("Han sido añadidas con exito");
     ShowProducts();
 }
 
+//Se crea un div con la lista de productos, el total de envio y el total a pagar
 function ShowProducts()
 {
     contenedor.remove();
